b75fc351151516a20c6768837f52723548199988
Anas Ahmad <76814594+anasahmd@users.noreply.github.com>
2021-06-29 10:14:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = 
+  `<div class="key">
+     <p>${e.key === ' '? 'Space': e.key}</p>
+     <small>event.key</small>
+  </div>
+  <div class="key">
+    <p>${e.keyCode}</p>
+    <small>event.keyCode</small>
+  </div>
+  <div class="key">
+    <p>${e.code}</p>
+    <small>event.code</small>
+  </div>`
+})
